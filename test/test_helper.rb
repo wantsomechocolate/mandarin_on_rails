2978c903183c7c4f1ea7aeb2638e715602ffa57b
James McGlynn <wantsomechocolate@gmail.com>
2022-10-19 11:21:43 -0400

    trying to minimize diff between e7 commit

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,6 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
-#require File.expand_path('../../config/environment', __FILE__)
 require "rails/test_help"
-#require "minitest/reporters"
-#Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
