4445f8d1df484c623ab4a18e15af1923da4793f2
James McGlynn <wantsomechocolate@gmail.com>
2022-10-20 18:56:25 -0400

    Ran rails g controller GarbageWords

---
+++ app/helpers/garbage_words_helper.rb
@@ -0,0 +1,2 @@
+module GarbageWordsHelper
+end
---
+++ test/controllers/garbage_words_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class GarbageWordsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
